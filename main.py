c9ae09b67e95941c1b362fc7c9f693a04ea74620
Narzgul <tzoockeer@gmail.com>
2024-02-01 10:22:22 +0100

    extract title & description and make headless

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
-driver = webdriver.Firefox()
+options = Options()
+options.add_argument('-headless')
+
+driver = webdriver.Firefox(options=options)
 driver.install_addon("uBlock0_1.55.0.firefox.signed.xpi")
-driver.get("https://www.kleinanzeigen.de/s-51647/anzeige:angebote/preis:200:300/rx-6700-xt/k0l1885r100")
+driver.get("https://www.kleinanzeigen.de/s-51647/anzeige:angebote/preis:200:450/rx-6700-xt/k0l1885r100")
 ad_list = driver.find_elements(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div[4]/div[2]/div[4]/div[1]/ul/li")
 for element in ad_list:
     print("----------------")
     print(element.text)
     try:
         print(
-            "Preis: " + element.find_element(by=By.CLASS_NAME, value="aditem-main--middle--price-shipping--price").text)
+            "Price: " + element.find_element(by=By.CLASS_NAME, value="aditem-main--middle--price-shipping--price").text)
+        print(
+            "Title: " + element.find_element(by=By.CLASS_NAME, value="ellipsis").text)
+        print(
+            "Description: " + element.find_element(by=By.CLASS_NAME, value="aditem-main--middle--description").text)
     except NoSuchElementException:
         print("Abtrenner")  # Element is just a gray seperator
 
